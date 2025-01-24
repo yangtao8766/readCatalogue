8fbe876011a67fc851a75f7535a812a586232c56
yang tao <1322147900@qq.com>
2025-01-24 17:09:53 +0800

    style: 优化代码格式 - 修改了 cli.js 文件中的 require 语句，将双引号改为单引号 - 更新了 package.json 文件中的版本号，从 1.0.17 升级到 1.0.18

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { readCatalogue } = require("../dist/common/chunk-index-C-AAJZ2K.cjs");
+const { readCatalogue } = require('../dist/common/chunk-index-C-AAJZ2K.cjs');
 const path = require("path");
 
 const aegs = process.argv.slice(2);
